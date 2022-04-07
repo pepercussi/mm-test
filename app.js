ac2abdb272a7019ea2260107f3030f7f5b5a8436
guido.pardo@boehringer-ingelheim.com <guido.pardo@boehringer-ingelheim.com>
2022-04-07 11:29:41 -0300

    Some code clean

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const cors = require('cors');
+
 var restRouter = require('./routes/rest');
-const storage = require('node-persist');
 
 var app = express();
-var wsApp = express();
 
 //CORS Setup
 app.use((req, res, next) => {
@@ -16,60 +14,11 @@ app.use((req, res, next) => {
   next();
 });
 
-
-//Create and set WS Server
-const serverWS = require('http').Server(wsApp);
-const WebSocketServer = require("websocket").server;
-
-const wsServer = new WebSocketServer({
-  httpServer: serverWS,
-  autoAcceptConnections: false
-});
-
-//Init storage
-const storageOptions = {
-  dir: './storage',
-  stringify: JSON.stringify,
-  parse: JSON.parse,
-  encoding: 'utf8',
-
-}
-
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-var wsPort = process.env.PORT_WS || '3030';
-wsApp.set("port", wsPort);
-wsApp.use(cors());
-wsApp.use(express.json());
-wsApp.use(express.static(path.join(__dirname, "./public")));
-
-// Here we get the web socket message sended by the client
-// We store it and send an answer back
-wsServer.on("request", async (request) =>{
-  await storage.init( storageOptions );
-  const connection = request.accept(null, request.origin);
-  connection.on("message", async (message) => {
-    var objMessage = JSON.parse(message.utf8Data);
-    var key = Object.keys(objMessage);
-    var value = Object.values(objMessage);
-    await storage.setItem(key[0], value[0]);
-    connection.sendUTF("Received: Key -> " + key[0] + " | Value -> " + value[0]);
-  });
-  connection.on("close", (reasonCode, description) => {
-      console.log("Client disconnected");
-  });
-});
-
-
-// Staarting the WS server in his current port
-serverWS.listen(wsApp.get('port'), () =>{
-  console.log('Websocket Server listening in port: ' + wsApp.get('port'));
-})
 
 app.use('/rest', restRouter);
 
--- controllers/restController.js
+++ controllers/restController.js
@@ -1,48 +1,46 @@
-const storage = require('node-persist');
-
-//Init storage
-const storageOptions = {
-    dir: './storage',
-    stringify: JSON.stringify,
-    parse: JSON.parse,
-    encoding: 'utf8',
-}
-
-
+var storage = require('node-persist');
 
 exports.getValueByKey = async function(req, res, next) {
-    await storage.init( storageOptions );
-    if(req.query.key){
-        var key = req.query.key;
-        var value = await storage.getItem(key);
-        var data = {
-            value: value
+    try {
+        if(req.query.key){
+            var key = req.query.key;
+            await storage.init();
+            var value = await storage.getItem(key);
+            var data = {
+                value: value
+            }
+            if(!value){
+                data.value = "";
+            }
+            res.send(data);
         }
-        if(!value){
-            data.value = "";
-        }
-        res.send(data);
+    } catch (error) {
+        res.send({status: "error", error: error});
     }
 };
 
 exports.getAllKeys = async function(req, res, next) {
-    await storage.init( storageOptions );
-    var data = {
-        status: 'empty',
-        arrayData: []
+    try {
+        await storage.init();
+        var data = {
+            status: 'empty',
+            arrayData: []
+        }
+        var arrKeys = await storage.keys();
+        if(arrKeys.length > 0){
+            data.status = 'ok';
+            data.arrayData = arrKeys;
+        }
+        res.send(data);
+    } catch (error) {
+        res.send({status: "error", error: error});
     }
-    var arrKeys = await storage.keys();
-    if(arrKeys.length > 0){
-        data.status = 'ok';
-        data.arrayData = arrKeys;
-    }
-    res.send(data);
 
 };
 
 exports.deleteAllKeys = async function(req, res, next) {
-    await storage.init( storageOptions );
     try {
+        await storage.init();
         await storage.clear();
         res.send({status: "ok"});
     } catch (error) {
